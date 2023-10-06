c4ca19d3f50d84f7872d3c8d34e198ea91c7357d
Vittoria <pierluigiciancio@gmail.com>
2023-10-06 21:51:24 +0200

    Removed unused files

--- assets/abstract/shaders/pipeline/pipeline.glsl
+++
@@ -1,9 +0,0 @@
-/******************************************************
-  abstract:shaders/pipeline/pipeline.glsl
-******************************************************/
-
-uniform ivec2 frxu_size;
-uniform int frxu_lod;
-uniform int frxu_layer;
-// Ortho projection matrix for full-frame processing
-uniform mat4 frxu_frameProjectionMatrix;
--- assets/abstract/shaders/pipeline/shadow.frag
+++
@@ -1,7 +0,0 @@
-/******************************************************
-  abstract:shaders/pipeline/shadow.frag
-******************************************************/
-
-void frx_pipelineFragment() {
-	gl_FragDepth = gl_FragCoord.z;
-}
